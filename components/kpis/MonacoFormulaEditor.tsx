8cef26722a8cf7b93c94773be108ce9a17db2deb
Francisco puente <fpuente@gcpglobal.com>
2025-11-15 14:37:28 -0600

    Fix: Add missing Editor import in MonacoFormulaEditor

--- components/kpis/MonacoFormulaEditor.tsx
+++ components/kpis/MonacoFormulaEditor.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useRef, useState } from 'react';
-import { OnMount } from '@monaco-editor/react';
+import Editor, { OnMount } from '@monaco-editor/react';
 import { extractVariablesFromFormula } from '@/lib/kpi-utils/formula-parser';
 
 interface MonacoFormulaEditorProps {
